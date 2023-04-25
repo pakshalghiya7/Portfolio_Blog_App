c229c6c3d633610334b35767019813606c5d9619
pakshalghiya7 <125532012+pakshalghiya7@users.noreply.github.com>
2023-04-25 10:50:18 +0530

    Add files via upload

---
+++ blogApp/projects/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Projects(models.Model):
+    title = models.CharField (max_length=50)
+    description = models.TextField()
+    technology = models.CharField(max_length=50)
+    image = models.FilePathField(path="/img")
+    
